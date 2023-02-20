0478ec590a4315aa9e046aa193ff69642bbcc7be
Suryaprakash Shanmugam <suryaprakash.shanmugam@intel.com>
2023-02-20 22:19:00 +0530

    [OVEP]  Add num_streams config option
    This high level streams configuration option in OpenVINO sets the number of parallel inference requests that can be run simultaneously

--- include/onnxruntime/core/session/onnxruntime_c_api.h
+++ include/onnxruntime/core/session/onnxruntime_c_api.h
@@ -565,9 +565,7 @@ typedef struct OrtMIGraphXProviderOptions {
  */
 typedef struct OrtOpenVINOProviderOptions {
 #ifdef __cplusplus
-  OrtOpenVINOProviderOptions() : device_type{}, enable_vpu_fast_compile{}, device_id{},
-                                 num_of_threads{}, cache_dir{},
-                                 context{}, enable_opencl_throttling{}, enable_dynamic_shapes{} {}
+  OrtOpenVINOProviderOptions() : device_type{}, enable_vpu_fast_compile{}, device_id{}, num_of_threads{}, cache_dir{}, num_streams{}, context{}, enable_opencl_throttling{}, enable_dynamic_shapes{} {}
 #endif
   /** \brief Device type string
    *
@@ -576,8 +574,9 @@ typedef struct OrtOpenVINOProviderOptions {
   const char* device_type;
   unsigned char enable_vpu_fast_compile;  ///< 0 = disabled, nonzero = enabled
   const char* device_id;
-  size_t num_of_threads;               ///< 0 = Use default number of threads
-  const char* cache_dir;          // path is set to empty by default
+  size_t num_of_threads;  ///< 0 = Use default number of threads
+  const char* cache_dir;  // path is set to empty by default
+  int num_streams;        ///< 1 = default, -1 = ov::streams::AUTO (let device decide), >1 = user defined number of parallel inference streams
   void* context;
   unsigned char enable_opencl_throttling;  ///< 0 = disabled, nonzero = enabled
   unsigned char enable_dynamic_shapes;     ///< 0 = disabled, nonzero = enabled
@@ -3919,7 +3918,7 @@ struct OrtApi {
   ORT_API2_STATUS(SessionOptionsAppendExecutionProvider_Dnnl,
                   _In_ OrtSessionOptions* options, _In_ const OrtDnnlProviderOptions* dnnl_options);
 
-   /** \brief Create an OrtDnnlProviderOptions
+  /** \brief Create an OrtDnnlProviderOptions
    *
    * \param[out] out Newly created ::OrtDnnlProviderOptions. Must be released with OrtApi::ReleaseDnnlProviderOptions
    *
