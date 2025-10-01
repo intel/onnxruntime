2ea59a97852036c19a2ca8c475ea11ad206a577d
Kotomi-Du <yaru.du@intel.com>
2025-10-01 12:13:38 -0700

    expose rewind api

--- onnxruntime/core/providers/openvino/openvino_execution_provider.cc
+++ onnxruntime/core/providers/openvino/openvino_execution_provider.cc
@@ -277,7 +277,7 @@ common::Status OpenVINOExecutionProvider::SetEpDynamicOptions(gsl::span<const ch
           }
         }
       }
-    } else if (key == "kvcache_rewind") {
+    } else if (key == "kvcache_rewind" || key == kOrtEpDynamicOptionsKVCacheRewind) {
       // Convert kvcache_rewind value to int64_t
       int64_t index;
       try {
