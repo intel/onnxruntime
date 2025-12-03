8291389ebbd9c0cc49dfccde2d2a96573cfb09fc
Jaswanth51 <jaswanth.gannamaneni@intel.com>
2025-12-02 22:09:41 -0800

    Revert "Sync with Microsoft ONNX Runtime - 03/12/2025 (#867)"
    This reverts commit 39d6db58a737449b0bd3ddc9b5a8777f4426d99d.

--- onnxruntime/core/providers/webgpu/webgpu_execution_provider.cc
+++ onnxruntime/core/providers/webgpu/webgpu_execution_provider.cc
@@ -794,7 +794,8 @@ using namespace webgpu;
 WebGpuExecutionProvider::WebGpuExecutionProvider(int context_id,
                                                  WebGpuContext& context,
                                                  WebGpuExecutionProviderConfig&& config)
-    : IExecutionProvider{kWebGpuExecutionProvider, WebGpuDevice},
+    : IExecutionProvider{kWebGpuExecutionProvider,
+                         OrtDevice(OrtDevice::GPU, OrtDevice::MemType::DEFAULT, OrtDevice::VendorIds::NONE, 0)},
       context_id_{context_id},
       context_{context},
       preferred_data_layout_{config.data_layout},
@@ -934,14 +935,13 @@ std::unique_ptr<onnxruntime::IExternalDataLoader> WebGpuExecutionProvider::GetEx
 std::optional<bool> WebGpuExecutionProvider::ShouldConvertDataLayoutForOp(std::string_view node_domain,
                                                                           std::string_view node_op_type,
                                                                           DataLayout target_data_layout) const {
-  // NHWC for Resize operator is not implemented on kWebGpuExecutionProvider
-  if (node_domain == kOnnxDomain && node_op_type == "Resize") {
-    return target_data_layout != DataLayout::NHWC;
+  if (target_data_layout != DataLayout::NHWC) {
+    return std::nullopt;
   }
 
-  // WebGPU perfer NCHW for InstanceNormalization due to a better performance
-  if (node_domain == kOnnxDomain && node_op_type == "InstanceNormalization") {
-    return target_data_layout != DataLayout::NHWC;
+  // NHWC for Resize operator is not implemented on kWebGpuExecutionProvider
+  if (node_domain == kOnnxDomain && node_op_type == "Resize") {
+    return false;
   }
 
   return std::nullopt;
