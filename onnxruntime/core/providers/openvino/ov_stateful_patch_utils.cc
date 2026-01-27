aa8b21e591dc064db05ef39043d1f4c514abf50a
Jaswanth51 <jaswanth.gannamaneni@intel.com>
2026-01-26 21:40:17 -0800

    Fixed Double declaration and merge Conflict

--- onnxruntime/core/providers/openvino/ov_stateful_patch_utils.cc
+++ onnxruntime/core/providers/openvino/ov_stateful_patch_utils.cc
@@ -81,10 +81,6 @@ void FuseCacheReorder(std::shared_ptr<ov::Model> ov_model,
     throw std::runtime_error("Model already has fused cache");
   }
 
-  if (ModelHasInputOutputNames(ov_model, "src_idx") || ModelHasInputOutputNames(ov_model, "dst_idx")) {
-    throw std::runtime_error("Model already has reorder feature for KV cache");
-  }
-
   // Define input name candidates in priority order
   const std::vector<std::string> input_name_candidates = {
       "inputs_embeds",                       // Default fallback
@@ -98,29 +94,11 @@ void FuseCacheReorder(std::shared_ptr<ov::Model> ov_model,
   auto input_batch = ov_model->input(main_input_name).get_partial_shape()[0];
   auto update_shape = ov_model->input(key_value_input_names[0]).get_partial_shape();
 
-  std::shared_ptr<ov::opset13::Parameter> beam_idx;
-  std::shared_ptr<ov::opset13::Parameter> src_idx;
-  std::shared_ptr<ov::opset13::Parameter> dst_idx;
-
-  if (should_add_kvcache_reorder) {
-    src_idx = std::make_shared<ov::opset13::Parameter>(ov::element::i32, ov::PartialShape({update_shape[2]}));
-    src_idx->set_friendly_name("src_idx");
-    src_idx->output(0).get_tensor().add_names({"src_idx"});
-    ov_model->add_parameters({src_idx});
-    not_kv_inputs.push_back(src_idx->get_friendly_name());
-
-    dst_idx = std::make_shared<ov::opset13::Parameter>(ov::element::i32, update_shape);
-    dst_idx->set_friendly_name("dst_idx");
-    dst_idx->output(0).get_tensor().add_names({"dst_idx"});
-    ov_model->add_parameters({dst_idx});
-    not_kv_inputs.push_back(dst_idx->get_friendly_name());
-  } else {
-    beam_idx = std::make_shared<ov::opset13::Parameter>(ov::element::i32, ov::PartialShape({std::move(input_batch)}));
-    beam_idx->set_friendly_name("beam_idx");
-    beam_idx->output(0).get_tensor().add_names({"beam_idx"});
-    ov_model->add_parameters({beam_idx});
-    not_kv_inputs.push_back(beam_idx->get_friendly_name());
-  }
+  auto beam_idx = std::make_shared<ov::opset13::Parameter>(ov::element::i32, ov::PartialShape({std::move(input_batch)}));
+  beam_idx->set_friendly_name("beam_idx");
+  beam_idx->output(0).get_tensor().add_names({"beam_idx"});
+  ov_model->add_parameters({beam_idx});
+  not_kv_inputs.push_back(beam_idx->get_friendly_name());
 
   std::shared_ptr<ov::opset13::Parameter> src_idx;
   std::shared_ptr<ov::opset13::Parameter> dst_idx;
@@ -144,23 +122,10 @@ void FuseCacheReorder(std::shared_ptr<ov::Model> ov_model,
     auto parameter_output_port = ov_model->input(input_name);
     auto consumers = parameter_output_port.get_target_inputs();
 
-    std::shared_ptr<ov::Node> output_node;
-    if (should_add_kvcache_reorder) {
-      auto updatekv_gather_op =
-          std::make_shared<ov::opset13::Gather>(parameter_output_port,
-                                                src_idx,
-                                                ov::opset13::Constant::create(ov::element::i64, {}, {2}));
-
-      auto updatekv_op = std::make_shared<ov::opset12::ScatterElementsUpdate>(parameter_output_port,
-                                                                              dst_idx,
-                                                                              updatekv_gather_op,
-                                                                              ov::opset13::Constant::create(ov::element::i64, {}, {2}));
-      output_node = updatekv_op;
-    } else {
-      output_node = std::make_shared<ov::opset13::Gather>(parameter_output_port,
-                                                          beam_idx,
-                                                          ov::opset13::Constant::create(ov::element::i64, {}, {gather_dim}));
-    }
+    auto gather_op =
+        std::make_shared<ov::opset13::Gather>(parameter_output_port,
+                                              beam_idx,
+                                              ov::opset13::Constant::create(ov::element::i64, {}, {gather_dim}));
 
     std::shared_ptr<ov::Node> output_node;
     if (should_add_kvcache_reorder) {
@@ -340,6 +305,7 @@ void PatchStatefulDecoder(std::shared_ptr<ov::Model> model, const bool should_ad
   auto batch_dim = 0;
 
   FuseCacheReorder(model, not_kv_inputs, key_value_input_names, batch_dim, should_add_kvcache_reorder);
+
   MakeStateful(model, key_value_input_names, key_value_output_names);
 }
 
